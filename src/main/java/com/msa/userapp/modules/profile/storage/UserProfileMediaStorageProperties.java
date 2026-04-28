eba4f9761c4a21de607bad29b3a1a1852151f396
myindiankisan <multisuperapp@gmail.com>
2026-04-28 23:10:04 +0530

    s3 bucket issues

--- src/main/java/com/msa/userapp/modules/profile/storage/UserProfileMediaStorageProperties.java
+++ src/main/java/com/msa/userapp/modules/profile/storage/UserProfileMediaStorageProperties.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Getter
 @Setter
 public class UserProfileMediaStorageProperties {
-    private String provider = "LOCAL";
+    private String provider = "S3";
     private String localRoot = System.getProperty("java.io.tmpdir") + "/msa-storage";
     private String awsRegion = "us-east-1";
     private final PublicMedia publicMedia = new PublicMedia();
@@ -17,12 +17,12 @@ public class UserProfileMediaStorageProperties {
     @Getter
     @Setter
     public static class PublicMedia {
-        private String bucket = "msa-public-media-dev";
+        private String bucket = "multisuperapp-bucket";
     }
 
     @Getter
     @Setter
     public static class Prefixes {
-        private String profile = "profile";
+        private String profile = "msa-public-media-dev/profiles/user";
     }
 }
